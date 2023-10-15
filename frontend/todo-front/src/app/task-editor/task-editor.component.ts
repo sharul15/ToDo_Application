4d744df04a1b0834338e147b3fb2be5ba9f9243b
SharulDhawane <sharuldhawane@gmail.com>
2023-10-15 07:12:00 +0530

    Added angular project

---
+++ frontend/todo-front/src/app/task-editor/task-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-editor',
+  templateUrl: './task-editor.component.html',
+  styleUrls: ['./task-editor.component.css']
+})
+export class TaskEditorComponent {
+
+}
